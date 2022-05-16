389a49a7f1cea8a159fe81bf3463d19ca64673bb
Ouafae MOUDNI <omoudni@e3r12p15.clusters.42paris.fr>
2022-05-16 23:46:46 +0200

    nithing left to have an operational tokenizer

--- minishell.h
+++ minishell.h
@@ -6,25 +6,28 @@
 /*   By: omoudni <omoudni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 14:31:25 by omoudni           #+#    #+#             */
-/*   Updated: 2022/05/15 21:29:31 by omoudni          ###   ########.fr       */
+/*   Updated: 2022/05/16 23:40:35 by omoudni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-typedef enum s_tok
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum s_tok_type
 {
 	TOK_IDK,
 	TOK_SEP,
 	TOK_QUOTER,
-	TOK_EXPANDER,
 	TOK_EXPANDER_OP,
 	TOK_EXPANDER_CL,
 	TOK_OPERATOR,
 	TOK_PATH,
 	TOK_WORD,
-} t_tok;
+	TOK_EXPANDER,
+} t_tok_type;
 
 typedef enum s_char_type
 {
@@ -65,7 +68,19 @@ typedef enum s_char_type
 	CHR_TILDA,
 } t_char_type;
 
-static t_char_type get_char_class[255] =
+typedef	struct 				s_token
+{
+	int						sp_before;
+//	int						sp_after;
+	int						length;
+	t_tok_type				token;
+	int						quoted;
+	struct s_token			*prev;
+	struct s_token			*next;
+}							t_token;
+
+
+static const t_char_type get_char_class[255] =
 	{
 		['\0'] = CHR_NULL,
 		['\t'] = CHR_SPACE,
@@ -73,16 +88,16 @@ static t_char_type get_char_class[255] =
 		['!'] = CHR_EXCLAM,
 		['"'] = CHR_D_QUOTE,
 		['#'] = CHR_COMMENT,
-		['\$'] = CHR_DOL,
+		['$'] = CHR_DOL,
 		['%'] = CHR_PER,
 		['&'] = CHR_AND,
 		['\''] = CHR_S_QUOTE,
-		['\('] = CHR_OP_PAREN,
-		['\)'] = CHR_CL_PAREN,
-		['\*'] = CHR_AST,
-		['\+'] = CHR_PLUS,
+		['('] = CHR_OP_PAREN,
+		[')'] = CHR_CL_PAREN,
+		['*'] = CHR_AST,
+		['+'] = CHR_PLUS,
 		['-'] = CHR_DASH_MINES,
-		['\.'] = CHR_POINT,
+		['.'] = CHR_POINT,
 		['/'] = CHR_SLASH,
 		['0' ... '9'] = CHR_NUM,
 		[':'] = CHR_COLON,
@@ -93,20 +108,20 @@ static t_char_type get_char_class[255] =
 		['?'] = CHR_INTEROG,
 		['@'] = CHR_AT,
 		['A' ... 'Z'] = CHR_ALPHA,
-		['\['] = CHR_OP_BRACKET,
+		['['] = CHR_OP_BRACKET,
 		['\\'] = CHR_ANTI_SLASH,
-		['\]'] = CHR_CL_BRACKET,
-		['\^'] = CHR_CIRCUM,
+		[']'] = CHR_CL_BRACKET,
+		['^'] = CHR_CIRCUM,
 		['_'] = CHR_UNDERSCORE,
 		['`'] = CHR_BACKTICK,
 		['a' ... 'z'] = CHR_ALPHA,
-		['\{'] = CHR_OP_BRACE,
-		['\|'] = CHR_PIPE,
-		['\}'] = CHR_CL_BRACE,
+		['{'] = CHR_OP_BRACE,
+		['|'] = CHR_PIPE,
+		['}'] = CHR_CL_BRACE,
 		['~'] = CHR_TILDA,
 };
 
-static t_tok get_tok_type[255] =
+static const t_tok_type get_tok_type[255] =
 	{
 		[CHR_NULL] = TOK_IDK,
 		[CHR_SPACE] = TOK_SEP,
@@ -139,12 +154,25 @@ static t_tok get_tok_type[255] =
 		[CHR_CL_BRACKET] = TOK_EXPANDER_CL,
 		[CHR_CIRCUM] = TOK_IDK,
 		[CHR_UNDERSCORE] = TOK_WORD,
-		[CHR_BACKTICK] = TOK_EXPANDER,
 		[CHR_ALPHA] = TOK_WORD,
 		[CHR_OP_BRACE] = TOK_EXPANDER_OP,
 		[CHR_PIPE] = TOK_OPERATOR,
 		[CHR_CL_BRACE] = TOK_EXPANDER_CL,
 		[CHR_TILDA] = TOK_PATH,
+		[CHR_BACKTICK] = TOK_IDK,
 };
+
+static	int	rules[9][255];
+
+//----------tokenizer.c--------------------------------------
+
+void ft_fill_tab(int n, int **tab, t_tok_type *get_tok_type);
+void fill_them_tables(int **tab, int size, t_tok_type *get_tok_type);
+t_token 	*ft_create_token(t_tok_type tok_type);
+void		add_tok_last(t_token **tok_list, t_tok_type tok_type, int length);
+int	is_quoted(t_token **tok_list, int rank_in_list);
+void	init_tok_struct(t_token **tok_list, int	rank_in_list, int length);
+
+//-----------
 
 #endif
--- tokenizer.c
+++ tokenizer.c
@@ -6,8 +6,190 @@
 /*   By: omoudni <omoudni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 14:30:49 by omoudni           #+#    #+#             */
-/*   Updated: 2022/05/15 14:30:56 by omoudni          ###   ########.fr       */
+/*   Updated: 2022/05/16 23:44:51 by omoudni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 
+void ft_fill_tab(int n, int **tab, t_tok_type *get_tok_type)
+{
+	int i;
+
+	i = 0;
+	while (i < 255)
+	{
+		if (get_tok_type[i] == n)
+			(*tab)[i] = 1;
+		else
+			(*tab)[i] = 0;
+		i++;
+	}
+}
+
+void fill_them_tables(int **tab, int size, t_tok_type *get_tok_type)
+{
+	printf("I entered here\n");
+	int i;
+	int *ptr;
+
+	i = 0;
+	while (i < size)
+	{
+		tab[i] = malloc(255 * sizeof(int));
+		ft_fill_tab(i, &(tab[i]), get_tok_type);
+		i++;
+	}
+}
+
+t_token 	*ft_create_token(t_tok_type tok_type)
+{
+	t_token	*tok;
+
+	tok = (t_token *)malloc(sizeof(t_token));
+	if (!tok)
+		return (NULL);
+	tok->token = tok_type;
+	tok->next = NULL;
+	return (tok);
+}
+
+void		add_tok_last(t_token **tok_list, t_tok_type tok_type, int length)
+{
+	t_token	*tmp;
+	t_token	*bef_last;
+	int		rank_in_list;
+
+	if (!*tok_list)
+	{
+		*tok_list = ft_create_token(tok_type);
+		(*tok_list)->prev = NULL;
+		rank_in_list = 0;
+	}
+	else
+	{
+		tmp = *tok_list;
+		while (tmp->next)
+			tmp = tmp->next;
+		bef_last = tmp;
+		bef_last->next = ft_create_token(tok_type);
+		bef_last = bef_last->next;
+		bef_last->prev = tmp;
+		rank_in_list = 1;
+	}
+	init_tok_struct(tok_list, rank_in_list, length);
+}
+
+int	is_quoted(t_token **tok_list, int rank_in_list)
+{
+	t_token 	*tmp;
+	int			res;
+
+	res = 1;
+	tmp = *tok_list;
+	if (!rank_in_list)
+		return (0);
+	else
+	{
+		while (tmp->prev)
+		{
+			tmp = tmp->prev;
+			if (tmp->token == TOK_QUOTER)
+				res *= -1;
+		}
+		if (res < 0)
+			return (1);
+		return (0);
+	}
+}
+
+void	init_tok_struct(t_token **tok_list, int	rank_in_list, int length)
+{
+	(*tok_list)->length = length;
+	if (is_quoted(tok_list, rank_in_list))
+		(*tok_list)->quoted = 1;
+	else
+		(*tok_list)->quoted = 0;
+	if (!rank_in_list)
+		(*tok_list)->sp_before = -1;
+	else
+		{
+			if ((*tok_list)->prev->token == TOK_SEP)
+				(*tok_list)->sp_before = 1;
+			(*tok_list)->sp_before = 0;
+		}
+}
+
+int	len_ll_list(t_token *tok_list)
+{
+	int	len;
+
+	len = 0;
+	if (!tok_list)
+		return (len);
+	while (tok_list)
+	{
+		len++;
+		tok_list = tok_list->next;
+	}
+	return (len);
+}
+
+unsigned int	get_real_tok_type(char c, t_token **tok_list)
+{
+	int	len;
+
+	len = len_ll_list(*tok_list);
+	if (len == 0 || len == 1)
+		return (get_tok_type[get_char_class[c]]);
+	else
+	{
+		if (is_quoted(tok_list, 1))
+			return (TOK_WORD);
+		else
+			return (get_tok_type[get_char_class[c]]);
+	}
+
+}
+
+void	detect_tokens(t_token **tok_list, char *str)
+{
+	int				i;
+	unsigned int	tok_type;
+	int				length;
+
+	i = 0;
+	length = 1;
+	if (!str)
+		return ;
+	while (str[i])
+	{
+		tok_type = get_tok_type[get_char_class[str[i]]];
+		i++;
+		while (str[i] && get_tok_type[get_char_class[str[i]]] == tok_type)
+		{
+			length++;
+			i++;
+		}
+
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	static	int	**rules;
+	int	i;
+	int	**ptr;
+	t_tok_type *tokens;
+
+	tokens = NULL;
+	i = 0;
+	rules = malloc(9 * sizeof(int *));
+	fill_them_tables(rules, 9, get_tok_type);
+	while (i < 255)
+	{
+		printf("%d", rules[0][i]);
+		i++;
+	}
+	return 0;
+}
