5826588c9c8c0f2ba5841bb860ab703b0e19b434
Nflan <niclas.flan91@gmail.com>
2022-06-29 12:17:57 +0200

    on s'attaque a du gros

--- srcs/executer.c
+++ srcs/executer.c
@@ -12,9 +12,9 @@
 
 #include "../include/minishell.h"
 
-int no_sib_has_child(t_big_token *b_tokens)
+int	no_sib_has_child(t_big_token *b_tokens)
 {
-	t_big_token *tmp_b;
+	t_big_token	*tmp_b;
 
 	tmp_b = b_tokens;
 	while (tmp_b)
@@ -35,26 +35,24 @@ int no_sib_has_child(t_big_token *b_tokens)
 	return (0);
 }
 
-int	ft_open_all_fdout(t_big_token *b_tokens, t_fd *tmp_fd)
+int	ft_open_all_fdout(t_big_token *b_tokens, t_fd *fd)
 {
 	char	*err;
 
 	err = NULL;
-	if (tmp_fd)
+	if (fd)
 	{
-		while (tmp_fd)
+		while (fd)
 		{
-		//	printf("file = %s\n", tmp_fd->file);
-		//	printf("red = %d\n", tmp_fd->red);
-			if (!tmp_fd->red)
-				tmp_fd->fd = open(tmp_fd->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (!fd->red)
+				fd->fd = open(fd->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			else
-				tmp_fd->fd = open(tmp_fd->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (tmp_fd->fd < 0)
+				fd->fd = open(fd->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd->fd < 0)
 			{
 				b_tokens->sc = 1;
-				tmp_fd->fd = 1;
-				err = ft_strjoin("minishell: ", tmp_fd->file);
+				fd->fd = 1;
+				err = ft_strjoin("minishell: ", fd->file);
 				if (!err)
 					return (1);
 				perror(err);
@@ -62,13 +60,13 @@ int	ft_open_all_fdout(t_big_token *b_tokens, t_fd *tmp_fd)
 				return (1);
 			}
 			else
-				b_tokens->fdout = tmp_fd->fd;
-			if (tmp_fd->next)
+				b_tokens->fdout = fd->fd;
+			if (fd->next)
 			{
-				close(tmp_fd->fd);
-				tmp_fd->fd = 0;
+				close(fd->fd);
+				fd->fd = 0;
 			}
-			tmp_fd = tmp_fd->next;
+			fd = fd->next;
 		}
 	}
 	return (0);
@@ -183,15 +181,15 @@ int	ft_exec_pipex(t_info *info, t_big_token *b_tokens, int *pid)
 int	ft_init_pipex(t_info *info, t_big_token *b_tokens)
 {
 	t_big_token	*tmp_b;
-	int	i;
+	int			i;
 
 	tmp_b = b_tokens;
 	i = 0;
 	while (tmp_b && ++i)
 		tmp_b = tmp_b->sibling;
 	info->pid = ft_calloc(sizeof(int), i);
-		if (!info->pid)
-			return (1);
+	if (!info->pid)
+		return (1);
 	tmp_b = b_tokens;
 	if (pipe(info->pdes) == -1)
 		return (ft_error(5, info, NULL));
@@ -213,14 +211,12 @@ int	ft_exec_simple(t_info *info, t_big_token *b_tokens)
 	t_big_token	*tmp_b;
 
 	tmp_b = b_tokens;
-//	print_tab(b_tokens->cmd_args);
 	if (ft_wash_btoken(info, tmp_b))
 		return (2147483647);
 	if (tmp_b->sc == -1)
 	{
 		if (ft_add_wildcards(info, b_tokens))
 			return (ft_putstr_error("Wildcards error\n"));
-	//	print_tab(b_tokens->cmd_args);
 		ft_launch_cmd(info, tmp_b);
 		tmp_b->sc = info->status;
 	}
@@ -242,7 +238,7 @@ int	ft_check_dol(char *str)
 
 int	ft_check_expand(t_token *token, int start, int length)
 {
-	t_token *tmp;
+	t_token	*tmp;
 
 	tmp = token;
 	move_tok_2_ind(&tmp, start);
@@ -261,62 +257,58 @@ int	ft_check_expand(t_token *token, int start, int length)
 	return (0);
 }
 
-int exec_the_bulk(t_info *info, int sib_child, t_big_token *b_tokens)
+int	exec_the_bulk(t_info *info, int sib_child, t_big_token *b)
 {
 	info->nb_cmd = 0;
-	if (!ft_open_fd(b_tokens))
+	if (!ft_open_fd(b))
 	{
-		if (!b_tokens->par)
+		if (!b->par)
 		{
-			if (ft_check_expand(info->tokens, b_tokens->ind_tok_start, b_tokens->length))
+			if (ft_check_expand(info->tokens, b->ind_tok_start, b->length))
 			{
-				dol_expand(&info->tokens, info, b_tokens);
-				expanded_toks(&info->tokens, b_tokens->ind_tok_start, b_tokens->length);
+				dol_expand(&info->tokens, info, b);
+				expanded_toks(&info->tokens, b->ind_tok_start, b->length);
 				index_toks(&info->tokens);
-				if (ft_noquote_args(b_tokens))
+				if (ft_noquote_args(b))
 					return (1);
 			}
-			print_tab(b_tokens->cmd_args);
+			print_tab(b->cmd_args);
 		}
 		if (sib_child >= 1 && sib_child <= 3)
-			ft_exec_simple(info, b_tokens);
+			ft_exec_simple(info, b);
 		else if (sib_child == 4)
-			ft_init_pipex(info, b_tokens);
+			ft_init_pipex(info, b);
 	}
-	ft_close_fd(b_tokens);
+	ft_close_fd(b);
 	return (0);
 }
 
 void	give_parent_sc(t_big_token **child, t_big_token **parent)
 {
-//	printf("tmpb parent 1 [0] = %s\n", child[0]->parent->cmd_args[0]);
 	(*parent)->sc = (*child)->sc;
-//	printf("tmpb parent 1 SC = %d\n", child[0]->parent->sc);
 	if ((*parent)->parent && (*parent)->par)
 		give_parent_sc(parent, &(*parent)->parent);
 }
 
 int	rec_exec(t_info *info, t_big_token **b_tokens, int and_or)
 {
-	t_big_token *tmp_b;
-	int i;
-	int fc;
+	t_big_token	*tmp_b;
+	int			i;
+	int			fc;
 
 	fc = 0;
 	i = 0;
 	tmp_b = *b_tokens;
-//	printf("b_tokens->type = %d\n", (*b_tokens)->type);
 	while (tmp_b)
 	{
-	//	printf("tmpb [0] = %s\n",tmp_b->cmd_args[0]);
 		if (tmp_b->child && tmp_b->sc == -1)
 		{
 			if (!tmp_b->par)
 				rec_exec(info, &(tmp_b->child), 0);
 		}
-		else if ((!i && no_sib_has_child(tmp_b) && tmp_b->parent && tmp_b->sc == -1 && tmp_b->type == TOK_LEFT_PIPE))
+		else if ((!i && no_sib_has_child(tmp_b) && tmp_b->parent
+				&& tmp_b->sc == -1 && tmp_b->type == TOK_LEFT_PIPE))
 		{
-//			printf("value b_token si sibling pas child: "); print_s_tokens(&info->tokens, tmp_b->ind_tok_start, tmp_b->length); printf("\n"); printf("je lance sibling sans child !! (start = %d && length = %d)\n", tmp_b->ind_tok_start, tmp_b->length);
 			if (exec_the_bulk(info, no_sib_has_child(tmp_b), tmp_b))
 				return (1);
 			if (tmp_b->parent)
@@ -325,7 +317,6 @@ int	rec_exec(t_info *info, t_big_token **b_tokens, int and_or)
 		}
 		if (tmp_b->type == TOK_PIPE_LAST)
 		{
-//				printf("value b_token dans le pipe last\n"); print_s_tokens(&info->tokens, tmp_b->ind_tok_start, tmp_b->length); printf("\nFC == 1 && b_tok->sc = %d\n", tmp_b->sc);
 			if (exec_the_bulk(info, 4, *b_tokens))
 				return (1);
 			if (tmp_b->parent)
@@ -336,18 +327,16 @@ int	rec_exec(t_info *info, t_big_token **b_tokens, int and_or)
 			fc = 1;
 		else if (tmp_b->type == TOK_LEFT_OR && i == and_or)
 			fc = 2;
-		else if (tmp_b->type == TOK_LAST || tmp_b->type == TOK_CLEAN || tmp_b->type == TOK_CLEAN_PIPED)
+		else if (tmp_b->type == TOK_LAST || tmp_b->type == TOK_CLEAN
+			|| tmp_b->type == TOK_CLEAN_PIPED)
 			fc = 3;
 		if (fc)
 			break ;
 		tmp_b = tmp_b->sibling;
 		i++;
 	}
-	if (tmp_b && tmp_b->sc == -1 && (!tmp_b->child || tmp_b->par)) //execute le bloc tmp_b tout seul and get the sc;
+	if (tmp_b && tmp_b->sc == -1 && (!tmp_b->child || tmp_b->par))
 	{
-	//	printf("value b_token (commande solo) && tmp_b->sc = %d\n", tmp_b->sc);
-	//	print_s_tokens(&info->tokens, tmp_b->ind_tok_start, tmp_b->length);
-	//	printf("\n");
 		if (exec_the_bulk(info, 1, tmp_b))
 			return (1);
 		if (tmp_b->parent)
@@ -355,9 +344,6 @@ int	rec_exec(t_info *info, t_big_token **b_tokens, int and_or)
 	}
 	if (tmp_b && fc > 0 && fc < 3)
 	{
-	//	printf("value b_token dans le FC (%d)\n", fc);
-	//	print_s_tokens(&info->tokens, tmp_b->ind_tok_start, tmp_b->length);
-	//	printf("\nb_tok->sc = %d\n", tmp_b->sc);
 		if ((fc == 1 && tmp_b->sc == 0) || (fc == 2 && tmp_b->sc))
 		{
 			rec_exec(info, b_tokens, and_or + 1);
@@ -382,6 +368,5 @@ int	rec_exec(t_info *info, t_big_token **b_tokens, int and_or)
 		if (tmp_b)
 			rec_exec(info, &(tmp_b), and_or);
 	}
-//	printf("je pars des parentheses\n");
 	return (0);
 }
