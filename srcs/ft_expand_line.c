f3a203593bdf96d2f43549b665103bb79588e973
Nicolas FLAN <nflan@e3r12p16.42.fr>
2022-07-22 15:20:27 +0200

    expand corr

--- srcs/ft_expand_line.c
+++ srcs/ft_expand_line.c
@@ -6,7 +6,7 @@
 /*   By: omoudni <omoudni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 13:11:41 by omoudni           #+#    #+#             */
-/*   Updated: 2022/07/21 18:26:39 by nflan            ###   ########.fr       */
+/*   Updated: 2022/07/22 15:18:23 by nflan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,9 +115,8 @@ char	*ft_expand_l(char *str, t_info *info, int hd)
 		if (str[i] == '$')
 		{
 			i++;
-			if (str[i] && (t < 2 || hd))// && (ft_isdigit(str[i]) || ft_isalpha(str[i]) || str[i] == '?'))
+			if (str[i] && (!t || hd || (t == 1 && str[i] != '\'')))
 			{
-				printf("str = %s && i = %d\n", str, i);
 				str = ft_expand_line(str, &i, info, t);
 				if (!str)
 					return (NULL);
