4d66c64341f63284821e427ab5833028c3fe4b8d
Ouafae MOUDNI <omoudni@e1r12p19.clusters.42paris.fr>
2022-07-24 22:21:52 +0200

    before merging

--- srcs/ft_fd_close.c
+++ srcs/ft_fd_close.c
@@ -6,7 +6,7 @@
 /*   By: omoudni <omoudni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 13:12:44 by omoudni           #+#    #+#             */
-/*   Updated: 2022/07/24 16:37:39 by nflan            ###   ########.fr       */
+/*   Updated: 2022/07/24 22:20:28 by omoudni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,5 @@ void	ft_close_fd(t_big_token *b_tokens)
 	{
 		if (b_tokens->fd)
 			ft_close_all_fd(b_tokens->fd);
-//		if (b_tokens->fd_in)
-//			ft_close_all_fd(b_tokens->fd_in, 0);
 	}
 }
